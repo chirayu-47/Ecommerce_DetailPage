d7946c3ed4c4ea62218c6d587d70a7ff2b2edd81
chirayu-47 <ck8161@gmail.com>
2022-05-25 17:54:08 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,20 @@ class MyApp extends StatelessWidget {
   }
 }
 
+Widget _colorWidget(Color color, {bool isSelected = false}) {
+  return CircleAvatar(
+    radius: 12,
+    backgroundColor: color.withAlpha(150),
+    child: isSelected
+        ? Icon(
+            Icons.check_circle,
+            color: color,
+            size: 18,
+          )
+        : CircleAvatar(radius: 7, backgroundColor: color),
+  );
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
@@ -34,71 +48,78 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-          children: [
-            Expanded(
-              child: Container(
-                margin: const EdgeInsets.fromLTRB(33.0, 48.0, 33.0, 0.0),
-                child: Column(
-                  children: [
-                    Stack(
-                      children: <Widget>[
-                        Image.asset(
-                          'images/LargeSofa.jpeg',
-                          //width: 348,
-                          //height: 277,
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              //color: Colors.amber,
+              margin: const EdgeInsets.fromLTRB(33.0, 48.0, 33.0, 0.0),
+              child: Column(
+                children: [
+                  Stack(
+                    children: <Widget>[
+                      Image.asset(
+                        'images/LargeSofa.jpeg',
+                        //width: 348,
+                        //height: 277,
+                      ),
+                      Positioned(
+                        top: 93.73,
+                        right: 284,
+                        //bottom: 272.2,
+                        left: 30.13,
+                        child: IconButton(
+                          icon: const Icon(Icons.arrow_back_ios),
+                          onPressed: () {},
+                          color: Colors.white,
+                          iconSize: 44,
                         ),
-                        Positioned(
-                          top: 93.73,
-                          right: 284,
-                          //bottom: 272.2,
-                          left: 30.13,
-                          child: IconButton(
-                            icon: const Icon(Icons.arrow_back_ios),
-                            onPressed: () {},
-                            color: Colors.white,
-                            iconSize: 44,
-                          ),
+                      ),
+                      Positioned(
+                        top: 93.73,
+                        right: 10.07,
+                        //bottom: 113.73,
+                        child: IconButton(
+                          icon: const Icon(Icons.arrow_forward_ios),
+                          onPressed: () {},
+                          color: Colors.white,
+                          iconSize: 44,
                         ),
-                        Positioned(
-                          top: 93.73,
-                          right: 10.07,
-                          //bottom: 113.73,
-                          child: IconButton(
-                            icon: const Icon(Icons.arrow_forward_ios),
-                            onPressed: () {},
-                            color: Colors.white,
-                            iconSize: 44,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 42,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Image.asset('images/SmallChair.jpeg'),
-                        const SizedBox(
-                          width: 20,
-                        ),
-                        Image.asset('images/SmallSofa.jpeg'),
-                      ],
-                    ),
-                  ],
-                ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 42,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Image.asset('images/SmallChair.jpeg'),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Image.asset('images/SmallSofa.jpeg'),
+                    ],
+                  ),
+                ],
               ),
             ),
-            Expanded(
+          ),
+          Expanded(
+            child: SingleChildScrollView(
               child: Container(
                 //color: Colors.amber,
-                width: double.infinity,
+                //width: double.infinity,
+                //height: double.infinity,
                 margin: const EdgeInsets.fromLTRB(57, 41, 59, 0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text('Floating Phone',style: TextStyle(fontSize: 20,fontWeight: FontWeight.w700),),
+                    const Text(
+                      'Floating Phone',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                    ),
                     const SizedBox(
                       height: 13.83,
                     ),
@@ -128,7 +149,8 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     const Text(
                       '\$1,139.33',
-                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
                     ),
                     const SizedBox(
                       height: 5,
@@ -159,15 +181,100 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     const Text(
                       'Met minim Mollie non desert Alamo est sit cliquey dolor do met sent. RELIT official consequent door ENIM RELIT Mollie. Excitation venial consequent sent nostrum met.',
-                      style: TextStyle(color: Colors.grey,fontWeight: FontWeight.w400,fontSize: 14,),
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14,
+                      ),
                     ),
-                    const SizedBox(height: 16,),
-                    const Divider(thickness: 2,)
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    const Divider(
+                      thickness: 2,
+                    ),
+                    const SizedBox(
+                      height: 19,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        _colorWidget(Colors.yellow, isSelected: true),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        _colorWidget(Colors.lightBlue),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        _colorWidget(Colors.black),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        _colorWidget(Colors.red),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        //_colorWidget(Colors.brown),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 48,
+                    ),
+                    Row(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Select Options'),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: const Color(0XffE8E8E8),
+                            ),
+                          ),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.favorite_border),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: const Color(0XffE8E8E8),
+                            ),
+                          ),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.shopping_cart,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 72,
+                    ),
                   ],
                 ),
               ),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
